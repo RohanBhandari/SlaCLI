b28133e0158fa7d6f64fdf569c80431265f8a534
Rohan Bhandari <rohan@physics.ucsb.edu>
2017-02-04 19:52:33 +0100

    Updated some argument parsing

--- slacli.py
+++ slacli.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 
 import argparse
+from argparse import ArgumentDefaultsHelpFormatter
 import os
 from channels import getChannels
 from read import readMessages
@@ -13,16 +14,15 @@ if __name__ == '__main__':
     parser.add_argument('-t','--token', help='Use specified token instead of $SLACK_TOKEN')
     subparsers = parser.add_subparsers(title='Commands', dest='subparser_name')
 
-    parser_l = subparsers.add_parser('list', help='List channels and/or users')
-    group_l = parser_l.add_mutually_exclusive_group(required=True)
-    group_l.add_argument('-a','--all', action='store_true', help='List channels and users')
-    group_l.add_argument('-c','--channels', action='store_true', help='List channels')
-    group_l.add_argument('-u','--users', action='store_true', help='List users')
-    
+    parser_l = subparsers.add_parser('list', help='List channels and users')
+    group_l = parser_l.add_mutually_exclusive_group(required=False)
+    group_l.add_argument('-c','--channels', action='store_true', help='List only channels')
+    group_l.add_argument('-u','--users', action='store_true', help='List only users')
+
     parser_r = subparsers.add_parser('read', help='Read messages from a channel')
     parser_r.add_argument('channel', help='The channel or user to read from')
     parser_r.add_argument('-u', '--unread', help='Show only unread messages')
-    parser_r.add_argument('num', nargs='?', default='10', help='Number of messages to display')
+    parser_r.add_argument('num', default='10', help='Number of messages to display. Default is 10')
 
     parser_s = subparsers.add_parser('send', help='Send a message')
     parser_s.add_argument('channel', help='The channel or user to send the message to')
@@ -36,7 +36,7 @@ if __name__ == '__main__':
     if args.subparser_name == 'list':
         if args.channels: getChannels(token)
         elif args.users: getUsers(token)
-        elif args.all: 
+        else: 
             getChannels(token) 
             getUsers(token)
 
